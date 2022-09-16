2403d79a85b4205f660823a4243458b31b6a1e72
yunaLee <yn.lee@okestro.com>
2022-09-16 14:29:56 +0900

    revert

--- src/main/java/com/okestro/server/ServerApplication.java
+++ src/main/java/com/okestro/server/ServerApplication.java
@@ -2,11 +2,8 @@ package com.okestro.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
